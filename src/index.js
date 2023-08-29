400e5262f3bc43237b2b7099f928aff6e1f20a76
CrAsHeR034 <128222450+CrAsHeR034@users.noreply.github.com>
2023-08-29 14:51:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number === 0) return "zero";
+
+    const units = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    function convertHundreds(n) {
+        if (n < 100) return convertTens(n);
+        return `${units[Math.floor(n / 100)]} hundred${
+            n % 100 !== 0 ? " " + convertTens(n % 100) : ""
+        }`;
+    }
+
+    function convertTens(n) {
+        if (n < 10) return units[n];
+        if (n < 20) return teens[n - 10];
+        return `${tens[Math.floor(n / 10)]}${
+            n % 10 !== 0 ? " " + units[n % 10] : ""
+        }`;
+    }
+
+    return convertHundreds(number);
+};
